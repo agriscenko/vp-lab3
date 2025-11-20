192a4e2055476573f93b538e5d1611e7c95be4da
agriscenko <andrey.griscenko@gmail.com>
2025-11-20 23:26:54 +0200

    Add WPF application with core functionality.

---
+++ Lab3.WpfApplication/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Lab3.WpfApplication.ViewModels;
+using System.Windows;
+
+namespace Lab3.WpfApplication;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private MainWindowViewModel _viewModel;
+    
+    public MainWindow()
+    {
+        InitializeComponent();
+        InitModel();
+    }
+
+    private void InitModel()
+    {
+        _viewModel = new MainWindowViewModel();
+
+        _viewModel.Load();
+
+        this.DataContext = _viewModel;
+    }
+}
---
+++ Lab3.WpfApplication/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,61 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Lab2.DataAccess;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace Lab3.WpfApplication.ViewModels;
+
+class MainWindowViewModel : ObservableObject, INotifyPropertyChanged
+{
+    private DepartmentDbContext _db;
+
+    public MainWindowViewModel()
+    {
+        _db = new DepartmentDbContext();
+        SelectDepartmentCommand = new RelayCommand(LoadEmployees);
+    }
+
+    private Department[] _departments;
+
+    public Department[] Departments => _departments;
+
+    public void Load()
+    {
+        _departments = _db.Departments.ToArray();
+    }
+
+    private List<Employee> _employees = new List<Employee>();
+
+    public List<Employee> Employees
+    {
+        get => _employees;
+        set
+        {
+            _employees = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public Department SelectedDepartment { get; set; }
+
+    public void LoadEmployees()
+    {
+        if (SelectedDepartment == null)
+        {
+            return;
+        }
+
+        Employees = _db.Employees.Where(e => e.Department.Id == SelectedDepartment.Id).ToList();
+    }
+
+    public ICommand SelectDepartmentCommand { get; }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string name = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
